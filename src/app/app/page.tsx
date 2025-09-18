7f2ef233f20121a3023cc30e8a2618f04d8855cb
Pablo S <cineschonffeldt@gmail.com>
2025-09-18 09:51:35 -0500

    Lift state from search-form.tsx to page.tsx where the Pokedex lives and present searchText info as a <p> element for testing purposes

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import ActionButons from '@/components/action-buttons';
 import AppTitle from '@/components/app-title';
 import ContentBlock from '@/components/content-block';
@@ -8,13 +11,20 @@ import SearchForm from '@/components/search-form';
 import SpriteContainer from '@/components/sprite-container';
 
 export default function App() {
+  // <-- state is now here
+  const [searchText, setSearchText] = useState('');
+
   return (
     <main className="px-30 py-20">
-      <ContentBlock className="h-[700px] flex flex-col gap-3 p-10  py-6 items-start bg-[#dc0a2d] shadow-[10px_10px_rgba(0,0,0,0.4)]">
+      <ContentBlock className="h-[700px] flex flex-col gap-3 p-10 py-6 items-start bg-[#dc0a2d] shadow-[10px_10px_rgba(0,0,0,0.4)]">
         <AppTitle />
-        <SearchForm />
+        {/* pass state + setter to SearchForm */}
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
         <ActionButons />
+
         <ContentBlock>
+          {/* now you can use the value here */}
+          <p>{searchText}</p>
           <PokemonTitle />
           <SpriteContainer />
           <PokemonType />
--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import { useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
-export default function SearchForm() {
-  const [searchText, setsearchText] = useState('');
+interface Props {
+  searchText: string;
+  setSearchText: Dispatch<SetStateAction<string>>;
+}
 
+export default function SearchForm({ searchText, setSearchText }: Props) {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log(`Text input on form is "${searchText}"`);
     e.preventDefault();
-
     if (!searchText) return;
+    console.log(`Text input on form is "${searchText}"`);
+    // We should add a toast presenting that use needs to add something on the input in order to search, either a number or a pokemon name
   };
 
   return (
@@ -17,7 +20,7 @@ export default function SearchForm() {
       <input
         className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
         value={searchText}
-        onChange={(e) => setsearchText(e.target.value)}
+        onChange={(e) => setSearchText(e.target.value)}
         placeholder="Search Pokémon by name or number"
         spellCheck={false}
       />
